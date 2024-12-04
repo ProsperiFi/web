99c9259092c9e872b536c20c0510ee9141f686c3
davidcraig <davidcraig-uk@protonmail.com>
2024-12-04 17:34:20 +0000

    Update colors.

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-roboto-slab)]">
       <main className="flex flex-col gap-8 row-start-2 md:items-center sm:items-start text-center">
-        <p className='text-4xl'>{t("tagline")}</p>
+        <p className='text-4xl text-orange'>{t("tagline")}</p>
       </main>
     </div>
   );
