811410b7ebb0e074198e6fb128a85e194d51a6ba
davidcraig <davidcraig-uk@protonmail.com>
2024-12-04 13:39:36 +0000

    Translate month

--- app/reports/live/profitloss/page.tsx
+++ app/reports/live/profitloss/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-// import t from '@/functions/t';
+import t from '@/functions/t';
 
 export default function ProfitLossLive() {
   const year = 2025
   const month = "january"
+  const monthTranslated = t("month_jan")
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 md:items-center sm:items-start text-center">
-        <h1>{year} {month} Profit and Loss</h1>
+        <h1>{year} {monthTranslated} {t("title_pnl", "finance")}</h1>
       </main>
     </div>
   );
